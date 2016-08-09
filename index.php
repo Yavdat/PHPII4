c61e6d81c4118711407ca1685b4afdd39a8a18f0
Yavdat92 <yavdat92@mail.ru>
2016-08-09 18:35:14 +0300

    +Controllers System

--- index.php
+++ index.php
@@ -2,9 +2,5 @@
 
 require __DIR__.'/autoload.php';
 
-$view = new \App\View();
-
-$view->title="My site";
-$view->news=\App\Models\News::findAll();
-
-$view->display(__DIR__.'/App/templates/index.php');
+$controller=new \App\Controllers\News();
+$controller->action('Index');
