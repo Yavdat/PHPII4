0c34bbaee55aab457de0db891adae760e78ae377
Yavdat92 <yavdat92@mail.ru>
2016-08-09 17:14:58 +0300

    LAZY LOAD

--- App/Models/News.php
+++ App/Models/News.php
@@ -11,6 +11,12 @@ namespace App\Models;
 
 use App\Model;
 
+/**
+ * Class News
+ * @package App\Models
+ * 
+ * @property \App\Models\Author $author
+ */
 class News
     extends Model
 {
@@ -20,5 +26,23 @@ class News
     public $title;
     public $lead;
     public $author_id;
+
+    /**
+     * LAZY LOAD
+     *
+     * @param $k
+     * @return null
+     */
+    
+    public function __get($k)
+    {
+        switch ($k) {
+            case 'author':
+                return Author::findById($this->author_id);
+                break;
+            default:
+                return null;
+        }
+    }
 
 }
--- test.php
+++ test.php
@@ -6,4 +6,4 @@ require __DIR__.'/autoload.php';
 //var_dump($authors);
 
 $news=\App\Models\News::findAll();
-var_dump($news);
+var_dump($news[2]->author);
